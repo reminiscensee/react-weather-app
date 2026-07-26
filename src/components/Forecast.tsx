8cbef5a9d5000617843e12a66052fdcf1dde8182
reminiscense <dima0911v@gmail.com>
2026-07-26 23:50:28 +0300

    feat: integrate weather condition emojis in forecast view
    - Create getWeatherEmoji helper to map WMO weather codes to UI emojis
    - Apply mapped emojis to the 24-hour horizontal scroll timeline
    - Fix index synchronization for weather codes array

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -2,8 +2,8 @@ import './Card.css'
 import type { GeoResult, weatherDataResult } from '../types'
 
 type CardProps = {
-    weather: GeoResult | null
-    weatherData: weatherDataResult | null
+    weather: GeoResult | null;
+    weatherData: weatherDataResult | null;
 }
 
 
@@ -20,15 +20,39 @@ export function Forecast({ weather, weatherData }: CardProps) {
     if (startIndex === -1) {
         return null;
     }
+    function getWeatherEmoji(code:number) {
+        switch (true) {
+            case (code >= 95 && code < 100):
+                return "⛈️";
+            case (code >= 61 && code < 80):
+                return "🌧️";
+            case (code >= 80 && code < 83):
+                return "❄️";
+            case (code >= 45 && code < 48):
+                return "🌫️";
+            case (code === 0):
+                return "☀️";
+            case (code === 3):
+                return "☁️";
+            case (code === 1):
+                return "🌤️";
+            case (code === 2):
+                return "⛅";
+            default:
+                return "❓";
+        }
+    }
 
-    
+
     return (
         <div className='hourly-card'>
             {weatherData.hourly.time.slice(startIndex, startIndex + 24).map((timeItem, index) => {
-                const temperatureIndex  = index + startIndex
+                const temperatureIndex = index + startIndex
+                const currentCode = weatherData.hourly.weather_code[index];
                 return (
                     <div key={timeItem} className='hourly-item'>
                         <span >{weatherData.hourly.temperature_2m[temperatureIndex]} °C</span>
+                        <p>{getWeatherEmoji(currentCode)}</p>
                         <p>{timeItem.split('T')[1]}</p>
                     </div>
                 )
