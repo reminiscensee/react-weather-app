f7f18b813c84ab06598dd68f37d576811de1a326
reminiscense <dima0911v@gmail.com>
2026-07-29 02:08:17 +0300

    feat: add error handling for API requests and tooltip styling
    - Added try-catch block in geoData to handle failed API requests
    - Added error state to App to manage error visibility
    - Created custom tooltip CSS animation for error messages

--- src/App.tsx
+++ src/App.tsx
@@ -11,23 +11,37 @@ import './App.css'
 function App() {
 
   const [weather, setWeather] = useState<GeoResult | null>(null);
-  const [weatherData, setWeatherData]  = useState<weatherDataResult | null>(null)
+  const [weatherData, setWeatherData] = useState<weatherDataResult | null>(null);
   const [view, setView] = useState<'current' | 'forecast'>('current');
+  const [error, setError] = useState<string | null>(null);
 
   const geoData: GeoDataFn = async (city) => {
-    const response = await axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`)
-    setWeather(response.data.results[0]);
+    setError(null)
+    if (!city) {
+      setError('Enter the city');
+      return
+    }
+    try {
+      const response = await axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`)
+      setWeather(response.data.results[0]);
 
-    const weatherResponse = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${response.data.results[0].latitude}&longitude=${response.data.results[0].longitude}&daily=weather_code,temperature_2m_max,apparent_temperature_max,wind_speed_10m_max,temperature_2m_min,apparent_temperature_min,sunrise,sunset,uv_index_max,precipitation_probability_max&hourly=relative_humidity_2m,temperature_2m,weather_code,wind_speed_10m,apparent_temperature,precipitation_probability,is_day&current=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,wind_speed_10m,is_day&timezone=auto`);
-    setWeatherData(weatherResponse.data);
+      const weatherResponse = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${response.data.results[0].latitude}&longitude=${response.data.results[0].longitude}&daily=weather_code,temperature_2m_max,apparent_temperature_max,wind_speed_10m_max,temperature_2m_min,apparent_temperature_min,sunrise,sunset,uv_index_max,precipitation_probability_max&hourly=relative_humidity_2m,temperature_2m,weather_code,wind_speed_10m,apparent_temperature,precipitation_probability,is_day&current=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,wind_speed_10m,is_day&timezone=auto`);
+      setWeatherData(weatherResponse.data);
+    } catch {
+      setError('City not found')
+    
+    }
   }
-  
+
   return (
     <>
-    <SearchBar geoData={geoData} />
-    <SwitchButtons view={view} setView={setView}/>
-    {view === 'current' && <CardCurrent weather={weather} weatherData={weatherData} />}
-    {view === 'forecast' && <Forecast weather={weather} weatherData={weatherData} />}
+      <SearchBar geoData={geoData} />
+
+      {error && <div className='error'>{error}</div>}
+
+      <SwitchButtons view={view} setView={setView} />
+      {view === 'current' && <CardCurrent weather={weather} weatherData={weatherData} />}
+      {view === 'forecast' && <Forecast weather={weather} weatherData={weatherData} />}
 
     </>
   )
